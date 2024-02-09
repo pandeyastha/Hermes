d0664af36d8eb35b89b443d670a6f71fea8c496b
sherlyastha <asthapandeycse9@gmail.com>
2024-02-10 01:30:35 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <p className="text-5xl font-bold text-indigo-500">
+       Hello Rohit Kashyap!
+    </p>
+  );
+}
