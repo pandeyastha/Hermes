36857771975f20bd194da57225f1f43081ef65c0
sherlyastha <asthapandeycse9@gmail.com>
2024-02-13 00:33:16 +0530

    dark mode activated

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <p className="text-5xl font-bold text-indigo-500">
-       Hello Rohit Kashyap!
-    </p>
-  );
-}
